bdeed7b3a0609593439dce29a57ff5c126e121d2
Bill ON <bill.on@epitech.eu>
2023-03-26 23:58:19 +0200

    [UPDATE] add comments to ease code comprehension

--- routes/chicken.js
+++ routes/chicken.js
@@ -38,9 +38,11 @@ router.put('/', async (req, res) => {
         steps: req.body.steps,
         isRunning: req.body.isRunning
       })
-    
+    // we first delete the Chicken to then replace it with the user's request
     try {
+        // Chicken deletion
         await Chicken.deleteOne(res.chicken)
+        // Chicken creation
         const newchicken = await chicken.save()
         res.status(201).json(newchicken)
       } catch (err) {
@@ -78,17 +80,22 @@ router.delete('/', async (req, res) => {
 
 router.post('/run', async (req, res) => {   
     try {
+        // retrieve the 1st chicken in the database
         const chick = await Chicken.find()
         chick = chick[0]
         const newchicken = await Chicken.updateOne(
+            // keep the same data except for the STEPS' increment
             {$set: {
                 name: chick.name,
                 birthday: chick.birthday,
                 weight: chick.weight,
-                steps: chick.steps,
+                steps: chick.steps + 1,
                 isRunning: chick.isRunning
             }}
         )
+        // NB : same modus operandi as PATCH but changing steps' value gives the following error message :
+        // "Assignment to constant variable"
+        // need to repair ASAP
         res.status(201).json(newchicken)
       } catch (err) {
         res.status(500).json({ message: err.message })
